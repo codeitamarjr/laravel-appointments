76f886fdfd24fe988db3a00f6ce7baf9454ac8bf
Itamar Junior <hello@itjunior.dev>
2024-12-18 16:05:00 +0100

    Fix notification class path in laravel-appointments configuration

--- config/laravel-appointments.php
+++ config/laravel-appointments.php
@@ -2,7 +2,7 @@
 
 return [
     'default_duration' => 30, // Default duration for events in minutes
-    'notification_class' => \Codeitamarjr\LaravelAppointments\Notifications\AppointmentNotification::class,
+    'notification_class' => \Codeitamarjr\LaravelAppointments\src\Notifications\AppointmentNotification::class,
     'models' => [
         'event' => \Codeitamarjr\LaravelAppointments\Models\Event::class,
         'slot' => \Codeitamarjr\LaravelAppointments\Models\Slot::class,
